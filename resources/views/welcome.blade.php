947c6699d6b79d68e93d625375261ebe6825add6
nachoDevOs <ignaciomolinaguzman20@gmail.com>
2025-12-01 21:56:36 -0400

    boton de whatsapp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{setting('site.title')}} - {{setting('site.description')}}</title>
+    
+            <!-- Favicon -->
+    <?php $admin_favicon = Voyager::setting('site.logo', ''); ?>
+    @if($admin_favicon == '')
+        <link rel="shortcut icon" href="{{ asset('images/icon.png') }}" type="image/png">
+    @else
+        <link rel="shortcut icon" href="{{ Voyager::image($admin_favicon) }}" type="image/png">
+    @endif
+
+    {{-- SEO --}}
+    <meta property="og:title"         content="{{ Voyager::setting('site.title') }}" />
+    <meta property="og:description"   content="{{ Voyager::setting('site.description') }}" />
+    <meta property="og:image"         content="{{ $admin_favicon == '' ? asset('images/icon.png') : Voyager::image($admin_favicon) }}" />
+
+
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
