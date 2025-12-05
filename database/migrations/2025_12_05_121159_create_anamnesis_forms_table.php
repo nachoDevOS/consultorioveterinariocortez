c5715f48d71448afce61e3b9a7131a9bb0d1cfab
nachoDevOs <ignaciomolinaguzman20@gmail.com>
2025-12-05 12:37:53 -0400

    dev

--- database/migrations/2025_12_05_121159_create_anamnesis_forms_table.php
+++ database/migrations/2025_12_05_121159_create_anamnesis_forms_table.php
@@ -13,7 +13,74 @@ return new class extends Migration
     {
         Schema::create('anamnesis_forms', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('pet_id')->nullable()->constrained('pets');
+            $table->foreignId('doctor_id')->nullable()->constrained('users');
+
+            // INFORMACIÓN GENERAL
+            $table->date('date')->nullable();
+
+            // IDENTIFICACIÓN DEL PACIENTE
+            $table->string('reproductive_status')->nullable();
+            $table->decimal('weight', 10,2)->nullable();
+            $table->string('identification')->nullable();
+
+            // MOTIVO DE CONSULTA
+            $table->text('main_problem')->nullable();
+            $table->string('evolution_time')->nullable();
+            $table->string('recent_changes')->nullable();
+
+            // HISTORIA CLÍNICA ACTUAL
+            $table->string('appetite')->nullable();
+            $table->string('water_intake')->nullable();
+            $table->string('activity')->nullable();
+            $table->string('urination')->nullable();
+            $table->string('defecation')->nullable();
+            $table->string('temperature')->nullable();
+            $table->string('heart_rate')->nullable();
+            $table->string('respiratory_rate')->nullable();
+
+            // HISTORIA CLÍNICA PREVIA
+            $table->string('previous_diseases')->nullable();
+            $table->string('previous_surgeries')->nullable();
+            $table->string('current_medications')->nullable();
+            $table->string('allergies')->nullable();
+            $table->string('vaccines')->nullable();
+            $table->string('deworming')->nullable();
+
+            // ALIMENTACIÓN
+            $table->string('diet_type')->nullable();
+            $table->string('diet_brand')->nullable();
+            $table->string('diet_frequency')->nullable();
+            $table->string('diet_recent_changes')->nullable();
+
+            // AMBIENTE Y MANEJO
+            $table->string('housing')->nullable();
+            $table->string('access_to_exterior')->nullable();
+            $table->string('stay_place')->nullable();
+            $table->text('cohabiting_animals')->nullable();
+            $table->text('toxic_exposure')->nullable();
+
+            // REPRODUCCIÓN
+            $table->string('females_repro')->nullable();
+            $table->string('males_repro')->nullable();
+            $table->string('repro_complications')->nullable();
+
+            // OBSERVACIONES ADICIONALES
+            $table->text('additional_observations')->nullable();
+
+
+
+
+            $table->smallInteger('status')->default(1);
+
+            $table->timestamps();            
+            $table->foreignId('registerUser_id')->nullable()->constrained('users');
+            $table->string('registerRole')->nullable();
+
+            $table->softDeletes();
+            $table->foreignId('deleteUser_id')->nullable()->constrained('users');
+            $table->string('deleteRole')->nullable();
+            $table->text('deleteObservation')->nullable();
         });
     }
 
