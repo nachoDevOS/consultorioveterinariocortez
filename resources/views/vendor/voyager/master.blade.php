ec9d1b953b389bc7258ff47349624a01f6bd2962
nachoDevOs <ignaciomolinaguzman20@gmail.com>
2025-12-10 23:45:22 -0400

    dev

--- resources/views/vendor/voyager/master.blade.php
+++ resources/views/vendor/voyager/master.blade.php
@@ -438,6 +438,8 @@ if (\Illuminate\Support\Str::startsWith(Auth::user()->avatar, 'http://') || \Ill
 <!-- Javascript Libs -->
 <script type="text/javascript" src="{{ voyager_asset('js/dataTable/dataTable.js') }}"></script>
 
+<script src="{{ asset('js/input-numberBlock.js') }}"></script>
+
 
 
 
